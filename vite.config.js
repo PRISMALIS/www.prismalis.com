e12ffb21167afdd0cf74846d215d8064ece4154c
og238 <126598251+0LG0S@users.noreply.github.com>
2025-06-02 02:07:11 +0200

    Simplify build configuration and fix deployment

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  server: { port: 3000 },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        format: 'es',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+    emptyOutDir: true
   }
 });
